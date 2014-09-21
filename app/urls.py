b9f79846a771354fee05b4f87b88ac0bdf33582f
Matt Riddle <mriddle89@gmail.com>
2014-09-21 17:59:24 -0500

    Add start of page on sorting

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 import brainiac.views
 
-urlpatterns = patterns('', url(r'^$', brainiac.views.index, name='index'))
+urlpatterns = patterns('', url(r'^$', brainiac.views.index, name='index'),
+                       url(r'^sorts', brainiac.views.sorts, name='sorts'))
--- brainiac/views.py
+++ brainiac/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def sorts(request):
+    return render(request, 'sorts.html')
