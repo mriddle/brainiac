ef1fa592f73f649350d98bad23ea5b7886ba35a5
Matt Riddle <mriddle89@gmail.com>
2014-09-23 07:57:55 -0500

    Add bubble sort step verification

--- brainiac/models/bubble_sort.py
+++ brainiac/models/bubble_sort.py
@@ -1,9 +1,17 @@
+import copy
+
+
 class BubbleSort:
 
-    def sort(self, to_sort):
+    def sort(self, to_sort, steps=[]):
         for i in range(len(to_sort) - 1):
             if to_sort[i] > to_sort[i + 1]:
                 to_sort[i], to_sort[i + 1] = to_sort[i + 1], to_sort[i]
-                self.sort(to_sort)
+                steps.append(copy.copy(to_sort))
+                self.sort(to_sort, steps)
         return to_sort
 
+    def verify_sort_steps(self, to_sort):
+        steps = []
+        self.sort(to_sort, steps)
+        return steps
