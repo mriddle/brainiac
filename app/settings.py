5929620dbc9e7d355bb1aff457c8ce19c86b018e
Matt Riddle <mriddle89@gmail.com>
2014-10-06 08:39:49 -0500

    Add logging config

--- app/settings.py
+++ app/settings.py
@@ -107,3 +107,41 @@ STATICFILES_DIRS = (
 
 # Set nose to default test runner
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'brainiac.log',
+            'formatter': 'verbose'
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+        'brainiac': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
