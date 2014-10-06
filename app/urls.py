a0c199b74ae986fc90896231183b231117bcd453
Matt Riddle <mriddle89@gmail.com>
2014-10-06 08:16:18 -0500

    Setup form submission and response
    Does not yet provide any useful feedback

--- app/urls.py
+++ app/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('brainiac.views',
                        url(r'^$', 'index'),
-                       url(r'^bubble-sort', 'bubble_sort'))
+                       url(r'^bubble-sort', 'bubble_sort'),
+                       url(r'^submit-bubble-sort-answer', 'bubble_submit'))
--- brainiac/views.py
+++ brainiac/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import brainiac.models.bubble_sort as sorter
 
 
 def index(request):
@@ -7,3 +8,14 @@ def index(request):
 
 def bubble_sort(request):
     return render(request, 'bubble_sort.html')
+
+
+def bubble_submit(request):
+    given_answer = request.POST.get('bubble_sort_answer')
+    given_problem = [4, 1, 3, 2]
+    bubble_sort = sorter.BubbleSort()
+    valid = bubble_sort.compare(given_problem, given_answer)
+    actual = None
+    if not valid:
+        actual = bubble_sort.sort_steps(given_problem)
+    return render(request, 'bubble_sort.html', locals())
