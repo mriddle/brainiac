6e8d21f7a931d86e7e95979993f844e223ae3c3c
Matt Riddle <mriddle89@gmail.com>
2014-10-06 09:01:08 -0500

    Validate users answer and inform of result
    Include actual answer in response if invalid

--- brainiac/views.py
+++ brainiac/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import brainiac.models.bubble_sort as sorter
+import ast
 
 
 def index(request):
@@ -11,11 +12,12 @@ def bubble_sort(request):
 
 
 def bubble_submit(request):
-    given_answer = request.POST.get('bubble_sort_answer')
-    given_problem = [4, 1, 3, 2]
+    try:
+        given_answer = ast.literal_eval(request.POST.get('bubble_sort_answer'))
+    except:
+        given_answer = request.POST.get('bubble_sort_answer')
     bubble_sort = sorter.BubbleSort()
-    valid = bubble_sort.compare(given_problem, given_answer)
-    actual = None
+    valid = bubble_sort.compare([4, 1, 3, 2], given_answer)
     if not valid:
-        actual = bubble_sort.sort_steps(given_problem)
+        actual = bubble_sort.sort_steps([4, 1, 3, 2])
     return render(request, 'bubble_sort.html', locals())
